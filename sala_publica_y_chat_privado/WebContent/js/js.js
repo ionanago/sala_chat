f0c211c800ff8ba6fbea9d9b87fafbe043a3b697
ionanago <51776373+ionanago@users.noreply.github.com>
2019-09-06 13:07:10 +0200

    ->

---
+++ sala_publica_y_chat_privado/WebContent/js/js.js
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+$(function() {
+	$("#sfa").click(function(e) {
+		e.preventDefault();
+
+	});
+	$(".BorrarLibrocomplejo").click(function(e) {
+		e.preventDefault();
+		var padre = $(this).parent().parent();
+		var id = padre.find("td:first-child").text();
+		var nombre = padre.find("td:nth-child(2)").text();
+		var codigo  = padre.find("td:nth-child(3)").text();
+		var precio = padre.find("td:nth-child(4)").text();
+			$.post("BorrarProductoServlet", {
+			id:id,nombre:nombre,codigo:codigo,precio:precio
+		}).done(function(e) {
+			padre.remove();
+		}).fail(function(e) {
+		});
+	});
+	
+	
+	
+	$(".BorrarLibroSencillo").click(function(e) {
+		e.preventDefault();
+		var padre = $(this).parent().parent();
+		
+		var titulo = padre.find("td:first-child").text();
+		var autor = padre.find("td:nth-child(2)").text();
+		var isbn = padre.find("td:nth-child(3)").text();
+		var img = padre.find("td:nth-child(4)").text();
+		console.log(titulo);
+		console.log(autor);
+		console.log(isbn);
+		console.log(img);
+			$.post("BorrarServlet", {
+			titulo:titulo,
+			autor:autor,
+			isbn:isbn,
+			img:img
+		}).done(function(e) {
+			
+			padre.remove();
+		}).fail(function(e) {
+		});
+		
+		
+		
+		
+	});
+	
+	
+//	$(".ModificarProductoServlet").click(function(e) {
+//		e.preventDefault();
+//		var padre = $(this).parent().parent();
+//		var id = padre.find("td:first-child").text();
+//		var nombre = padre.find("td:nth-child(2)").text();
+//		var codigo  = padre.find("td:nth-child(3)").text();
+//		var precio = padre.find("td:nth-child(4)").text();
+//		$.post("ModificarProductoServlet", {
+//			id:id,
+//			nombre:nombre,
+//			codigo:codigo,
+//			precio:precio
+//		}).done(function(e) {
+//			padre.remove();
+//		}).fail(function(e) {
+//		});
+ // });
+})
+
+function editar(t,e) {
+	e.preventDefault();
+	var padre = $(t).parent().parent(), i = 2;
+	padre.find("td:not(:last-child)").each(function(){
+		
+		$("#formMod:nth-child(" + i + ") input").val($(this).html());
+		console.log($("#formMod div:nth-child(" + i + ") input").val($(this).html()));
+		
+		i++;
+	});
+};
--- sala_publica_y_chat_privado/src/sala_publica_y_chat_privado/clasepatata.java
+++
@@ -1,5 +0,0 @@
-package sala_publica_y_chat_privado;
-
-public class clasepatata {
-
-}
