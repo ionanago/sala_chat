f4ada746bdb5c3750d2c89456a172e516bf1041e
ionanago <51776373+ionanago@users.noreply.github.com>
2019-09-09 10:45:58 +0200

    -->

--- sala_publica_y_chat_privado/src/com/ipartek/formacion/chat/modelo/chats.java
+++ sala_publica_y_chat_privado/src/com/ipartek/formacion/chat/modelo/chats.java
@@ -1,5 +1,14 @@
 package com.ipartek.formacion.chat.modelo;
 
+import java.sql.Date;
+
 public class chats {
 
+	
+	private long id;
+	private String nombre;
+	private Date fecha;
+	
+	
+	
 }
--- sala_publica_y_chat_privado/src/com/ipartek/formacion/chat/modelo/usuarios.java
+++ sala_publica_y_chat_privado/src/com/ipartek/formacion/chat/modelo/usuarios.java
@@ -1,5 +1,71 @@
 package com.ipartek.formacion.chat.modelo;
 
 public class usuarios {
+private long id;
+private String nombre;
+
+
+public usuarios(long id, String nombre) {
+	super();
+	setId(id);
+	setNombre(nombre);
+}
+
+
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + (int) (id ^ (id >>> 32));
+	result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+	return result;
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	usuarios other = (usuarios) obj;
+	if (id != other.id)
+		return false;
+	if (nombre == null) {
+		if (other.nombre != null)
+			return false;
+	} else if (!nombre.equals(other.nombre))
+		return false;
+	return true;
+}
+
+@Override
+public String toString() {
+	return "usuarios [id=" + id + ", nombre=" + nombre + "]";
+}
+
+
+
+
+
+
 
 }
